9fa0de511d2a62e55e00304f6954e564ebf6a120
Félix Francisco <ffer200395@gmail.com>
2021-05-27 20:00:55 +0200

    -Filtrado y normalizado

--- RNA-seq workflow.R
+++ RNA-seq workflow.R
@@ -1,12 +1,13 @@
 ## --- Paquetes necesarios para el análisis
 if (!require(DESeq2))BiocManager::install("DESeq2")
+if(!(require(dplyr))) install.packages("dplyr")
 
 ## --- Cargamos los datos
 counts <- read.csv('data/counts.csv', sep = ';', header = T, row.names = 1)
 targets <- read.csv('data/targets.csv', sep = ',', header = T)
 
 # --- Creación del subconjunto del análisis (usar datos de expresión RNA-Seq) y preparación de los datos
-library(dyplr)
+library(dplyr)
 set.seed(1234)
 # Filtramos para tener sólo datos RNA Seq (NGS) y elegimos 10 muestras de cada grupo
 my.targets <- targets %>% filter(molecular_data_type == 'RNA Seq (NGS)') %>% group_by(Group) %>% sample_n(8)
@@ -32,12 +33,27 @@ dds <- DESeq(ddsMat)
 
 # --- Preprocesado de los datos: filtraje y normalización
 
-# Eliminamos filas del objeto DESeqDataSet que tienen un conteo de 0 o 1 en las muestras
+# Nos quedamos con aquellos genes que tengan al menos 10 reads en total
 nrow(dds)
-dds <- dds[ rowSums(counts(dds)) > 1, ]
+dds <- dds[ rowSums(counts(dds)) >= 10, ]
 nrow(dds)
 
-# Normalizamos...........
+# Número de reads por muestra expresado en millones
+sort(colSums(my.counts))/1e6
+
+# Normalizamos
+my.counts.norm <- log2(counts(dds, normalized=TRUE)+1)
+
+# Mostramos los datos normalizados versus sin normalizar
+par(mfrow=c(1,2))
+plot(my.counts[,1:2], main='Muestra 1 vs muestra 2 sin normalizar')
+plot(my.counts.norm[,1:2],  main='Muestra 1 vs muestra 2 tras normalizar')
+par(mfrow=c(1,2))
+boxplot(log2(counts(dds)+1), main='Muestras sin normalizar', names=my.targets$SRA_Sample, las=2)
+boxplot(my.counts.norm, main='Muestras normalizadas', names=my.targets$SRA_Sample, las=2)
+
+
+
 
 
 
