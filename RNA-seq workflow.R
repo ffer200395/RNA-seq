bfbc62eb1cbb7887e536eb2470f93040994e9cc5
Félix Francisco <ffer200395@gmail.com>
2021-06-03 22:05:34 +0200

    -Pendiente de revisar código y comentarios

--- RNA-seq workflow.R
+++ RNA-seq workflow.R
@@ -3,6 +3,7 @@ if (!require(DESeq2))BiocManager::install("DESeq2")
 if (!(require(dplyr))) install.packages("dplyr")
 if (!require(VennDiagram)) install.packages("VennDiagram")
 if (!require(pheatmap)) install.packages("pheatmap")
+if (!require(GOstats))BiocManager::install("GOstats")
 
 ## --- Cargamos los datos
 counts <- read.csv('data/counts.csv', sep = ';', header = T, row.names = 1)
@@ -103,11 +104,7 @@ res_eli_sfi_Sig <- subset(res_eli_sfi, padj < 0.1)
 # Counts plot
 #A quick way to visualize the counts for a particular gene is to use the plotCounts
 par(mfrow=c(1,3),mar=c(2,2,2,2))
-plotCounts(dds, gene = rownames(res_sfi_nit_Sig)[which.min(res_sfi_nit_Sig$padj)], intgroup=c("Group"), main = 'SFI vs NIT')
-plotCounts(dds, gene = rownames(res_eli_nit_Sig)[which.min(res_eli_nit_Sig$padj)], intgroup=c("Group"), main = 'ELI vs NIT')
-plotCounts(dds, gene = "ENSG00000211654.2", intgroup=c("Group"), main = 'ELI vs SFI')
-#plotCounts(dds, gene = rownames(res_eli_sfi_Sig)[which.min(res_eli_sfi_Sig$padj)], intgroup=c("Group"), main = 'ELI vs SFI')
-
+plotCounts(dds, gene = rownames(res_sfi_nit_Sig)[which.min(res_sfi_nit_Sig$padj)], intgroup=c("Group"))
 
 # p-val hist
 par(mfrow=c(1,3),mar=c(2,2,2,2))
@@ -144,9 +141,27 @@ res_eli_sfi_Sig@nrows
 
 # --- Patrones de expresión y agrupación de las muestras
 
+# Volcano plot
+get.volcano <- function(data, pval, fc, title){
+  df = as.data.frame(data@listData)
+  # Inicialmente asumimos que no se expresan
+  df$diffexpressed <- "NO"
+  # Si el pvalor es inferior a pval y el foldchange es mayor de fc se considera UP
+  df$diffexpressed[df$log2FoldChange > fc & df$pvalue < pval] <- "UP"
+  # Si el foldchange es menor que -fc entonces se considera DOWN
+  df$diffexpressed[df$log2FoldChange < -fc & df$pvalue < pval] <- "DOWN"
+  # Creamos el volcano plot
+  ggplot(data=df, aes(x=log2FoldChange, y=-log10(padj), col=diffexpressed)) + geom_point() + geom_text(aes(label=symbol),check_overlap=T)+ ggtitle(title) + theme(plot.title = element_text(hjust = 0.5))
+}
+
+get.volcano(res_sfi_nit_Sig, 0.05, 2.5, 'Volcano plot SFI_NIT')
+get.volcano(res_eli_nit_Sig, 0.05, 2.5, 'Volcano plot ELI_NIT')
+get.volcano(res_eli_sfi_Sig, 0.05, 2.5, 'Volcano plot ELI_SFI')
+
 # Diagrama de Venn para visualizar el número de genes en común
 library(VennDiagram)
 get.num.inter <- function(data1,data2){return(length(intersect(as.array(row.names(data1)),as.array(row.names(data2)))))}
+dev.off()
 draw.triple.venn(area1 = nrow(res_eli_nit_Sig), area2 = nrow(res_eli_sfi_Sig), area3 = nrow(res_sfi_nit_Sig), 
                  n12 = get.num.inter(res_eli_nit_Sig,res_eli_sfi_Sig), 
                  n23 = get.num.inter(res_eli_sfi_Sig,res_sfi_nit_Sig), 
@@ -166,34 +181,16 @@ df <- as.data.frame(dds@colData[,c("Group")])
 rownames(df) <- colnames(mat)
 pheatmap(mat, annotation_col=df)
 
+# --- Análisis de significación biológica
+library(GOstats)
 
+# Almacenamos todo el conjunto de genes y los genes más diferenciados según un umbral del p-valor
+entrezUni = unique(res_eli_nit_Sig$entrez)
+whichgenes = which(res_eli_nit_Sig$padj<0.01)
+geneIds = unique(res_eli_nit_Sig[whichgenes,]$entrez)
 
-# Volcano plot
-library(ggplot2)
-test = as.data.frame(res_eli_nit_Sig@listData)
-ggplot(data=test, aes(x=test$log2FoldChange, y=-log10(test$padj))) + geom_point()
-# add a column of NAs
-test$diffexpressed <- "NO"
-# if log2Foldchange > 0.6 and pvalue < 0.05, set as "UP" 
-test$diffexpressed[test$log2FoldChange > 0.6 & test$pvalue < 0.05] <- "UP"
-# if log2Foldchange < -0.6 and pvalue < 0.05, set as "DOWN"
-test$diffexpressed[test$log2FoldChange < -0.6 & test$pvalue < 0.05] <- "DOWN"
+# Definimos los hyper parámetros para realizar las búsquedas
+paramsGO <- new("GOHyperGParams", geneIds =geneIds, universeGeneIds = entrezUni, annotation = "hgu133plus2.db", ontology = "BP", testDirection = "over", pvalueCutoff=0.01) 
 
-ggplot(data=test, aes(x=log2FoldChange, y=-log10(padj), col=diffexpressed)) + 
-  geom_point() + 
-  theme_minimal() +
-  geom_text()
-#plot(x=test$log2FoldChange, y=-log10(test$padj), data=test)
-#geom_point(aes(x=log2FoldChange, y=-log10(padj), colour=0.01)) 
-
-#PCA
-my.pca <- function(datos, grupos){
-  pca_res <- prcomp(t(assay(datos)), scale = FALSE)
-  pca_df <- as.data.frame(pca_res$x)
-  pca_df$Group <- grupos
-  pcts <- round(pca_res$sdev^2/sum(pca_res$sdev^2)*100,1)
-  plot(x = pca_df$PC1, y = pca_df$PC2, col = pca_df$Group, xlab = paste0('PC1 ',pcts[1],'%'), ylab = paste0('PC2 ',pcts[2],'%'))
-  legend("bottomleft", legend = unique(pca_df$Group), col = unique(pca_df$Group), pch =19, horiz = T,y.intersp=0.5,x.intersp=0.5,text.width=0.1,bty = 'n')
-}
-my.pca(rld, my.targets$Group)
-my.pca(vsd, my.targets$Group)
+# Instanciamos ambos tests
+hypGO <- hyperGTest(paramsGO)
